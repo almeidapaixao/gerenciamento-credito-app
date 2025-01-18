8a63d4387f28d165db29f7f4476584f1963d45f0
Almeida Paixão <almeidapaixao04@gmail.com>
2025-01-18 01:34:42 -0300

    urls dos endpoints e configuracao do router da view de contrato

--- gerenciamento_credito_app/urls.py
+++ gerenciamento_credito_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .app.views import ContratoViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .app.views import ContratoViewSet, TokenObtainFor30DaysView
 
 
 router = DefaultRouter()
@@ -13,5 +13,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/30days/', TokenObtainFor30DaysView.as_view(), name='token_obtain_30days'),
 ]
